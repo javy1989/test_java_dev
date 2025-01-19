eaee22d87023bbc4c6f5db2aca21d119be87f122
javy1989 <javier.tec1989@gmail.com>
2025-01-19 18:54:04 -0500

    Initial commit
    Initial commit

---
+++ ms-management/src/main/java/com/rbravo/ms_management/MsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.rbravo.ms_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsManagementApplication.class, args);
+	}
+
+}
---
+++ ms-management/src/test/java/com/rbravo/ms_management/MsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbravo.ms_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
